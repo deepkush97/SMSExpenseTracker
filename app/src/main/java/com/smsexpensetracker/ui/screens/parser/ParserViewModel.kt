8bca5f0add18a32a9ffbb2084d220950e9ae4126
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2026-07-30 22:27:51 +0530

    feat(viewmodel): add Hilt ViewModel stubs for all screens

---
+++ app/src/main/java/com/smsexpensetracker/ui/screens/parser/ParserViewModel.kt
@@ -0,0 +1,8 @@
+package com.smsexpensetracker.ui.screens.parser
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ParserViewModel @Inject constructor() : ViewModel()
---
+++ app/src/main/java/com/smsexpensetracker/ui/screens/transactions/TransactionsViewModel.kt
@@ -0,0 +1,8 @@
+package com.smsexpensetracker.ui.screens.transactions
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TransactionsViewModel @Inject constructor() : ViewModel()
